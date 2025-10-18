5138f62a14df3673f859d75bab91ccb76eff8818
hootbee <xx3xx27@gmail.com>
2025-10-19 03:28:11 +0900

    add: 회원가입 컴포넌트 생성

---
+++ client/src/SignUp.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function SignUp() {
+  return (
+    <div>
+      <h2>회원가입</h2>
+      <form>
+        <div>
+          <label htmlFor="name">이름: </label>
+          <input type="text" id="name" name="name" />
+        </div>
+        <div style={{ marginTop: '10px' }}>
+          <label htmlFor="age">나이: </label>
+          <input type="number" id="age" name="age" />
+        </div>
+        <div style={{ marginTop: '10px' }}>
+          <label htmlFor="username">ID: </label>
+          <input type="text" id="username" name="username" />
+        </div>
+        <div style={{ marginTop: '10px' }}>
+          <label htmlFor="password">비밀번호: </label>
+          <input type="password" id="password" name="password" />
+        </div>
+        <div style={{ marginTop: '20px' }}>
+          <button type="submit">회원가입하기</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default SignUp;
