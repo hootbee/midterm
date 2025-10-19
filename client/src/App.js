55751a1190f015b07068f04f420ac9085e6c6ef9
hootbee <xx3xx27@gmail.com>
2025-10-19 16:53:24 +0900

    add: 회원가입 버튼 생성

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,36 @@
 
+import React, { useState } from 'react';
 import './App.css';
+import Login from './Login';
+import SignUp from './SignUp';
 
 function App() {
+  const [showLogin, setShowLogin] = useState(false);
+  const [showSignUp, setShowSignUp] = useState(false);
+
+  const handleShowLogin = () => {
+    setShowLogin(true);
+    setShowSignUp(false);
+  };
+
+  const handleShowSignUp = () => {
+    setShowLogin(false);
+    setShowSignUp(true);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          <button>로그인</button>
-        </div>
+        {showLogin ? (
+          <Login />
+        ) : showSignUp ? (
+          <SignUp />
+        ) : (
+          <div>
+            <button onClick={handleShowLogin}>로그인</button>
+            <button onClick={handleShowSignUp} style={{ marginLeft: '10px' }}>회원가입</button>
+          </div>
+        )}
       </header>
     </div>
   );
