5a8b67bfcaca5885c9bfd0643987c9fda45bfb31
hootbee <xx3xx27@gmail.com>
2025-10-19 18:57:07 +0900

    add(server): db연결부분 추가

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const express = require('express');
+const connectMongo = require('./config/mongo');
 const { createUsersTable } = require('./models/userModel');
 const userRoutes = require('./routes/userRoutes');
+const auctionRoutes = require('./routes/auctionRoutes');
 
 // Initialize Express app
 const app = express();
@@ -9,11 +12,16 @@ const port = 3001;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-// Initialize Database
-createUsersTable();
+// Serve static files from the 'uploads' directory
+app.use('/uploads', express.static('uploads'));
+
+// Initialize Databases
+createUsersTable(); // For MariaDB
+connectMongo(); // For MongoDB
 
 // API Routes
 app.use('/api/users', userRoutes);
+app.use('/api/auctions', auctionRoutes);
 
 // Default route
 app.get('/', (req, res) => {
